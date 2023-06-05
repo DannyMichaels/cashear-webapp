3d86928d3071f0255be83bfe9c6005e10234c581
daniel <itzdanielmichael@gmail.com>
2023-06-05 15:09:55 -0400

    create pie chart

---
+++ components/PieChart.jsx
@@ -0,0 +1,184 @@
+import React, { useState } from 'react';
+import Pie from '@visx/shape/lib/shapes/Pie';
+import { Group } from '@visx/group';
+import { animated, useTransition, interpolate } from '@react-spring/web';
+
+const defaultMargin = { top: 20, right: 20, bottom: 20, left: 20 };
+const donutThickness = 50;
+
+export default function PieChart({
+  data,
+  itemIdentifierKey,
+  itemValueKey,
+  width,
+  height,
+  margin = defaultMargin,
+  getColor,
+  animate = true,
+}) {
+  const [selectedElement, setSelectedElement] = useState(null);
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+  const radius = Math.min(innerWidth, innerHeight) / 2;
+  const centerY = innerHeight / 2;
+  const centerX = innerWidth / 2;
+  const innerRadius = 70;
+
+  // display the selected element's label and value in the center of the pie
+  const selectedElementLabel =
+    selectedElement && selectedElement[itemIdentifierKey];
+
+  const selectedElementJSX = (
+    <text
+      style={{
+        fontSize: '1.5em',
+      }}
+      is="x3d"
+      fill="white"
+      x={centerX - margin.left}
+      y={centerY + margin.top}
+      dy=".33em"
+      textAnchor="middle"
+      pointerEvents="none">
+      {selectedElementLabel}
+    </text>
+  );
+
+  return (
+    <svg width={width} height={height}>
+      <Group top={centerY + margin.top} left={centerX + margin.left}>
+        <Pie
+          data={data}
+          pieValue={(item) => item[itemValueKey]}
+          pieSortValues={() => -1}
+          outerRadius={radius - donutThickness * 1.3}
+          innerRadius={innerRadius}
+          cornerRadius={6}>
+          {(pie) => (
+            <AnimatedPie
+              {...pie}
+              animate={animate}
+              radius={radius}
+              getKey={({ data }) => data[itemIdentifierKey]}
+              selectedElement={selectedElement}
+              itemIdentifierKey={itemIdentifierKey}
+              onClickDatum={(arc) => {
+                setSelectedElement(arc.data);
+              }}
+              getColor={getColor}
+            />
+          )}
+        </Pie>
+      </Group>
+      {selectedElement && selectedElementJSX}
+    </svg>
+  );
+}
+
+function AnimatedPie({
+  animate,
+  arcs,
+  path,
+  getKey,
+  getColor,
+  onClickDatum,
+  selectedElement,
+  itemIdentifierKey,
+  radius,
+}) {
+  const fromLeaveTransition = ({ endAngle }) => {
+    return {
+      // enter from 360° if end angle is > 180°
+      padAngle: endAngle > Math.PI ? 0.01 : 0.005,
+      startAngle: endAngle > Math.PI ? 2 * Math.PI : 0,
+      endAngle: endAngle > Math.PI ? 2 * Math.PI : 0,
+      opacity: 0,
+      outerRadius: radius - donutThickness * 1.3, // Default outer radius
+    };
+  };
+  const enterUpdateTransition = ({ startAngle, endAngle, data }) => {
+    const isSelected =
+      selectedElement &&
+      selectedElement[itemIdentifierKey] === getKey({ data });
+
+    // const padAngle = isSelected ? 0.1 : 0.02; // Reduce the pad angle for the selected slice
+    // const angleOffset = isSelected ? 0.1 : 0.0; // Increase the angle offset for a larger selected slice
+
+    return {
+      // padAngle,
+      // startAngle: startAngle - angleOffset,
+      // endAngle: endAngle + angleOffset,
+      startAngle,
+      endAngle,
+      opacity: 1,
+    };
+  };
+
+  const transitions = useTransition(arcs, {
+    from: animate ? fromLeaveTransition : enterUpdateTransition,
+    enter: enterUpdateTransition,
+    update: enterUpdateTransition,
+    leave: animate ? fromLeaveTransition : enterUpdateTransition,
+    keys: getKey,
+  });
+
+  return transitions((props, arc, { key }) => {
+    // const [centroidX, centroidY] = path.centroid(arc);
+    // const hasSpaceForLabel = arc.endAngle - arc.startAngle >= 0.1;
+
+    const isSelected =
+      selectedElement &&
+      selectedElement[itemIdentifierKey] === getKey({ data: arc.data });
+
+    const sliceSizeMultiplier = isSelected ? 1.2 : 1.0; // Increase the size of the selected slice
+    const sliceElevation = isSelected ? 8 : 0; // Add elevation to the selected slice
+
+    return (
+      <g key={key} is="x3d">
+        <animated.path
+          is="x3d"
+          // compute interpolated path d attribute from intermediate angle values
+          d={interpolate(
+            [props.startAngle, props.endAngle, props.padAngle],
+            (startAngle, endAngle, padAngle) => {
+              return path({
+                ...arc,
+                padAngle,
+                startAngle,
+                endAngle,
+              });
+            }
+          )}
+          fill={getColor(arc)}
+          onClick={() => onClickDatum(arc)}
+          onTouchStart={() => onClickDatum(arc)}
+          style={{
+            transform: `scale(${sliceSizeMultiplier}) translateZ(${sliceElevation}px)`,
+
+            transition: 'transform 0.2s ease-in-out',
+          }}
+        />
+        {/* <>
+          {hasSpaceForLabel && (
+            <animated.g style={{ opacity: props.opacity }}>
+              <text
+                is="x3d"
+                fill="white"
+                x={centroidX}
+                y={centroidY}
+                dy=".33em"
+                fontSize={9}
+                textAnchor="middle"
+                pointerEvents="none">
+                {key}
+              </text>
+            </animated.g>
+          )}
+        </> */}
+
+        <></>
+      </g>
+    );
+  });
+}
--- src/screens/RoyaltiesChart.jsx
+++ src/screens/RoyaltiesChart.jsx
@@ -1,7 +1,76 @@
 import React from 'react';
+import PieChart from '../../components/PieChart';
+import { scaleOrdinal } from '@visx/scale';
 
 function RoyaltiesChart() {
-  return <div>RoyaltiesChart</div>;
+  const earnings = {
+    'Apple Music': 1000,
+    Youtube: 2000,
+    Amazon: 3000,
+    Spotify: 4000,
+  };
+
+  const percentageOfEarning = (val) => {
+    if (!earnings) return;
+    const totalEarnings = Object.values(earnings).reduce(
+      (acc, curr) => acc + curr,
+      0
+    );
+    const percentage = (val / totalEarnings) * 100;
+
+    return percentage;
+  };
+
+  const data = [
+    {
+      label: 'Apple Music',
+      value: percentageOfEarning(earnings['Apple Music']),
+    },
+    { label: 'Youtube', value: percentageOfEarning(earnings['Youtube']) },
+    { label: 'Amazon', value: percentageOfEarning(earnings['Amazon']) },
+    { label: 'Spotify', value: percentageOfEarning(earnings['Spotify']) },
+  ];
+
+  const itemIdentifierKey = 'label';
+  const itemValueKey = 'value';
+
+  const colors = {
+    Spotify: '#00ff00',
+    'Apple Music': '#aab',
+    Amazon: '#ffff00',
+    Youtube: '#ff0000',
+  };
+
+  const getMyColor = scaleOrdinal({
+    domain: Object.keys(colors),
+    range: Object.values(colors),
+  });
+
+  const getColor = ({ data }) => {
+    const { label } = data;
+    return getMyColor(label);
+  };
+
+  return (
+    <main
+      style={{
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <>
+        <PieChart
+          getColor={getColor}
+          width={500}
+          height={500}
+          data={data}
+          itemIdentifierKey={itemIdentifierKey}
+          itemValueKey={itemValueKey}
+        />
+      </>
+    </main>
+  );
 }
 
 export default RoyaltiesChart;
