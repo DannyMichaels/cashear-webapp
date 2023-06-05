2b9c0e846ba58c893dd7d76f10824e853185e7ee
daniel <itzdanielmichael@gmail.com>
2023-06-05 12:39:12 -0400

    create routes

---
+++ src/navigation/router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import RoyaltiesChart from '../screens/RoyaltiesChart';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/royalties-chart',
+    element: <RoyaltiesChart />,
+  },
+]);
---
+++ src/screens/RoyaltiesChart.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function RoyaltiesChart() {
+  return <div>RoyaltiesChart</div>;
+}
+
+export default RoyaltiesChart;
